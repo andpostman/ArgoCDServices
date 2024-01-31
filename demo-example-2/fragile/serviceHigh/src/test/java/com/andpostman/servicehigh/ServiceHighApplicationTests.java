a5802ecf490b724e0f5fb305b1e6e63b3e1ff283
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-01 00:08:16 +0300

    Add for fragile version serviceHigh default test

---
+++ demo-example-2/fragile/serviceHigh/src/test/java/com/andpostman/servicehigh/ServiceHighApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.servicehigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHighApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
