557a6b524dd265f5bd5c84b9552f2e03d951f5e4
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-31 23:49:45 +0300

    Add for fragile version serviceMedium business logic service

---
+++ demo-example-2/fragile/serviceMedium/src/main/java/com/andpostman/servicemedium/service/ArithmeticService.java
@@ -0,0 +1,7 @@
+package com.andpostman.servicemedium.service;
+
+import com.andpostman.servicemedium.properties.RandomNumber;
+
+public interface ArithmeticService {
+    RandomNumber subtractionAndMultiplicationWithNumber(RandomNumber number);
+}
