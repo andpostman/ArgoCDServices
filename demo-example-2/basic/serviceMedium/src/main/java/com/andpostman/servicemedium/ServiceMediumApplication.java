f6bcf08a0a1f08c0ce844448c813f77fd7895dec
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-31 22:42:23 +0300

    Add for basic version serviceMedium application runner

---
+++ demo-example-2/basic/serviceMedium/src/main/java/com/andpostman/servicemedium/ServiceMediumApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.servicemedium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMediumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMediumApplication.class, args);
+    }
+
+}
