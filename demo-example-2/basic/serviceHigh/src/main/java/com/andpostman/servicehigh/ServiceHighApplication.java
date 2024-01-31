b39012e4314d0c480c85195c8c75c722518b804c
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-31 22:48:02 +0300

    Add for basic version serviceHigh application runner

---
+++ demo-example-2/basic/serviceHigh/src/main/java/com/andpostman/servicehigh/ServiceHighApplication.java
@@ -0,0 +1,13 @@
+package com.andpostman.servicehigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHighApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHighApplication.class, args);
+    }
+
+}
