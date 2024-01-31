ddfd182d958cd759ae29c824b79cbebdec6be80c
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-31 23:05:11 +0300

    Add for fragile version serviceLow rest controller test

---
+++ demo-example-2/fragile/serviceLow/src/test/java/com/andpostman/servicelow/controller/ArithmeticRestRequestHandlerTest.java
@@ -0,0 +1,52 @@
+package com.andpostman.servicelow.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import com.andpostman.servicelow.properties.RandomNumber;
+import com.andpostman.servicelow.service.ArithmeticServiceImpl;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestRequestHandler.class)
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+public class ArithmeticRestRequestHandlerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ArithmeticServiceImpl arithmeticService;
+
+    @Test
+    @DisplayName("Проверка когда тело и хедр заданы")
+    public void whenRequestBodyAndRequestHeaderAreNotEmpty_thenReturn200() throws Exception {
+        RandomNumber randomNumber = new RandomNumber();
+        mockMvc.perform(post("/serviceLow/multiply")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(randomNumber)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Проверка когда тело и хедр задан")
+    public void whenRequestBodyNotEmptyButRequestHeaderNotEmpty_thenReturn200() throws Exception {
+        RandomNumber randomNumber = new RandomNumber();
+        mockMvc.perform(post("/serviceLow/divide")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(randomNumber)))
+                .andExpect(status().isOk());
+    }
+}
