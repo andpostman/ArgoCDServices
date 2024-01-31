d1c22ad346bf1bb204e8afedc210d4f0bf33253f
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-31 23:54:45 +0300

    Add for fragile version serviceMedium default test

---
+++ demo-example-2/fragile/serviceMedium/src/test/java/com/andpostman/servicemedium/ServiceMediumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.servicemedium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMediumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
