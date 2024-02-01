9458f931031cf4cbcb84feba34816e4306346e82
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-01 23:28:22 +0300

    Add for corrupted version serviceA business runtime connection exception

---
+++ demo-example-1/corruptedVersion/ServiceA/src/main/java/com/andpostman/servicea/exception/BusinessConnectionException.java
@@ -0,0 +1,9 @@
+package com.andpostman.servicea.exception;
+
+public class BusinessConnectionException extends RuntimeException{
+
+    public BusinessConnectionException(String message) {
+        super(message.substring(message.lastIndexOf("Service"),message.lastIndexOf(";")));
+    }
+
+}
