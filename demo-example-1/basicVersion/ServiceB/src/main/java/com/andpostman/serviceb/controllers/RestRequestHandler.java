bea0e89e0d80500d3b74171c6c0f009f2fa9863a
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-01 16:18:36 +0300

    Add for basic version serviceB rest controller

---
+++ demo-example-1/basicVersion/ServiceB/src/main/java/com/andpostman/serviceb/controllers/RestRequestHandler.java
@@ -0,0 +1,29 @@
+package com.andpostman.serviceb.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+import com.andpostman.serviceb.dto.RandomNumber;
+import com.andpostman.serviceb.service.RandomNumberGenerationServiceImpl;
+
+@RestController("RestRequestHandlerServiceBv1")
+@Slf4j
+@RequiredArgsConstructor
+public class RestRequestHandler {
+
+    private final RandomNumberGenerationServiceImpl numberGenerationService;
+
+    @PostMapping("prepare")
+    public void prepare(@RequestHeader @Nullable HttpHeaders headers){
+        log.info("Подготовка ServiceB");
+    }
+
+    @GetMapping("get")
+    public RandomNumber get(@RequestHeader @Nullable HttpHeaders headers) {
+        log.info("Вызов ServiceB");
+        return numberGenerationService.generateNumber();
+    }
+
+}
