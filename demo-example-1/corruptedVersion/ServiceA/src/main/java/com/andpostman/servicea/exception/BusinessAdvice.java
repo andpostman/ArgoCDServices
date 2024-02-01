1c4456819c8edd8c1afda54ac950a8f0d7efcb2e
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-01 23:27:23 +0300

    Add for corrupted version serviceA exception handler

---
+++ demo-example-1/corruptedVersion/ServiceA/src/main/java/com/andpostman/servicea/exception/BusinessAdvice.java
@@ -0,0 +1,29 @@
+package com.andpostman.servicea.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class BusinessAdvice {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(BusinessConnectionException.class)
+    public String emptyFieldsHandler(BusinessConnectionException exception){
+        log.error(exception.getMessage());
+        return exception.getMessage();
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(BusinessLogicalException.class)
+    public String logicalErrorHandler(BusinessLogicalException exception){
+        log.error(exception.getMessage());
+        return exception.getMessage();
+    }
+}
