7a53d823a43ddce3c15ebf3ea63e8e1acafada53
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-01 22:50:16 +0300

    Add for basic version serviceD business logic service

---
+++ demo-example-1/basicVersion/ServiceD/src/main/java/com/andpostman/serviced/service/RandomNumberGenerationService.java
@@ -0,0 +1,7 @@
+package com.andpostman.serviced.service;
+
+import com.andpostman.serviced.dto.RandomNumber;
+
+public interface RandomNumberGenerationService {
+    RandomNumber generateNumber();
+}
