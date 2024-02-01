ee19e2ac52b12a0e169355cc4edb3498e202e928
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-01 16:03:03 +0300

    Add for basic version serviceA request and response body type

---
+++ demo-example-1/basicVersion/ServiceA/src/main/java/com/andpostman/servicea/dto/RandomNumber.java
@@ -0,0 +1,29 @@
+package com.andpostman.servicea.dto;
+
+import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import java.util.SplittableRandom;
+
+@Data
+public class RandomNumber {
+
+    private int number;
+    private String comment;
+
+    public RandomNumber() {
+        this.number = new SplittableRandom().nextInt(1,6);
+        this.comment = "выполнено";
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append(number)
+                .append(comment)
+                .toString();
+    }
+
+}
+
+
