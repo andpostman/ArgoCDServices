fdd73e3f77659ce185720e3624e282f8e7cf76ad
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-02 10:57:35 +0300

    Add for fragile version serviceA web client config

---
+++ demo-example-1/fragileVersion/ServiceA/src/main/java/com/andpostman/servicea/config/WebClientConfig.java
@@ -0,0 +1,36 @@
+package com.andpostman.servicea.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${web-client.service-b.base-url}")
+    private String serviceHostB;
+    @Value("${web-client.service-c.base-url}")
+    private String serviceHostC;
+
+    @Bean
+    public WebClient callServiceC() {
+        return WebClient.builder()
+                .baseUrl(serviceHostC)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+
+    @Bean
+    public WebClient callServiceB() {
+
+        return WebClient.builder()
+                .baseUrl(serviceHostB)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+}
