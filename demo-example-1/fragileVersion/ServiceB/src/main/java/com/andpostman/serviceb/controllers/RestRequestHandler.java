cf6314085c00dda113279fe03326166f4cdc67c9
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-02 11:10:02 +0300

    Add for fragile version serviceB rest controller

---
+++ demo-example-1/fragileVersion/ServiceB/src/main/java/com/andpostman/serviceb/controllers/RestRequestHandler.java
@@ -0,0 +1,29 @@
+package com.andpostman.serviceb.controllers;
+
+import com.andpostman.serviceb.dto.RandomNumber;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+import com.andpostman.serviceb.service.RandomNumberGenerationImpl;
+
+@RestController("RestRequestHandlerServiceBv3")
+@RequiredArgsConstructor
+@Slf4j
+public class RestRequestHandler {
+
+    private final RandomNumberGenerationImpl numberGenerationService;
+
+    @PostMapping("prepare")
+    public void prepare(@RequestHeader @Nullable HttpHeaders headers){
+        log.info("Подготовка ServiceB");
+    }
+
+    @GetMapping("get")
+    public RandomNumber get(@RequestHeader @Nullable HttpHeaders headers) {
+        log.info("Вызов ServiceB");
+        return numberGenerationService.generateNumber();
+    }
+
+}
