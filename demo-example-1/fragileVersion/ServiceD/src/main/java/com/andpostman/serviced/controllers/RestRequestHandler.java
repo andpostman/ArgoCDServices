70f532c3ea15e553daba8267e0dc3ed0147392b4
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-02 11:33:33 +0300

    Add for fragile version serviceD rest controller

---
+++ demo-example-1/fragileVersion/ServiceD/src/main/java/com/andpostman/serviced/controllers/RestRequestHandler.java
@@ -0,0 +1,24 @@
+package com.andpostman.serviced.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+import com.andpostman.serviced.dto.RandomNumber;
+import com.andpostman.serviced.service.RandomNumberGenerationServiceImpl;
+
+@RestController("RestRequestHandlerServiceDv3")
+@RequiredArgsConstructor
+@Slf4j
+public class RestRequestHandler {
+
+    private final RandomNumberGenerationServiceImpl numberGenerationService;
+
+    @GetMapping("get")
+    public RandomNumber get(@RequestHeader @Nullable HttpHeaders headers) {
+        log.info("Вызов ServiceD");
+        return numberGenerationService.generateNumber();
+    }
+
+}
