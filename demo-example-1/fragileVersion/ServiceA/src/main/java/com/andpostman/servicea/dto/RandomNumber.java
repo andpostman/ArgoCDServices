04eecdbfd3c8ed3cd063d208d8e7a34f0232fee9
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-02 10:59:09 +0300

    Add for fragile version serviceA request and response body type

---
+++ demo-example-1/fragileVersion/ServiceA/src/main/java/com/andpostman/servicea/dto/RandomNumber.java
@@ -0,0 +1,37 @@
+package com.andpostman.servicea.dto;
+
+import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.SplittableRandom;
+
+@Data
+public class RandomNumber{
+
+    private int number;
+    private String time;
+    private String comment;
+
+    public RandomNumber() {
+        this.number = new SplittableRandom().nextInt(1,6);
+        this.comment = "выполнено";
+    }
+
+    public void setTime() {
+        this.time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("Result:")
+                .append(number)
+                .append(time)
+                .append(comment)
+                .toString();
+    }
+}
+
+
