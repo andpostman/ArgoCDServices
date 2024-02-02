35f461cc03e276a68075514aa8d6dc3f22f2e536
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-02 11:01:19 +0300

    Add for fragile version serviceA business runtime connection exception

---
+++ demo-example-1/fragileVersion/ServiceA/src/main/java/com/andpostman/servicea/exception/BusinessConnectionException.java
@@ -0,0 +1,8 @@
+package com.andpostman.servicea.exception;
+
+public class BusinessConnectionException extends RuntimeException{
+
+    public BusinessConnectionException(String message) {
+        super(message.substring(message.lastIndexOf("Service"),message.lastIndexOf(";")));
+    }
+}
